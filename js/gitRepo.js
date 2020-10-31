27a7c6a8f6cfc790b1ef5b512c53969a28340924
meetxpress <pmeet7895@gmail.com>
2020-10-31 14:23:49 +0530

    "Added Gitfolio"

--- js/gitRepo.js
+++ js/gitRepo.js
@@ -3,9 +3,8 @@ request.open('GET', 'https://api.github.com/users/meetxpress/repos', true)
 request.onload = function() {
     var data = JSON.parse(this.response);
     var statusHTML = '';
-    console.log(data);
-    
-    $.each(data, function(i, status){
+    console.log(data);             
+    $.each(data, function(i, status){        
         statusHTML +='<div class="service-col">';
             statusHTML +='<div class="service-item content-box">';
                 if(status.fork == true){
@@ -18,16 +17,22 @@ request.onload = function() {
                         statusHTML +='<img src="https://img.icons8.com/ios-filled/22/4bffa5/repository.png" style="vertical-align:middle;"/>';
                         statusHTML +='<span class="text" style="font-size:20px; vertical-align:top; font-weight: bold; color: #4bffa5; padding-left:3px">'+ status.name +'</span>';
                     statusHTML +='</a></div>'; 
-                }                   
+                }                
                 statusHTML +='<div class="name">';
                     statusHTML +='<span style="margin-right: 15px;">';
                         statusHTML +='<img src="https://img.icons8.com/ios-filled/22/4bffa5/source-code.png" style="vertical-align:middle;"/>';
-                        statusHTML +='<span class="text" style="font-size:18px; vertical-align:top; padding-left:3px">'+ status.language+'</span>';
+                        if(status.language == null){                            
+                            statusHTML +='<span class="text" style="font-size:18px; vertical-align:top; padding-left:3px">Not Found</span>';
+                        } else{
+                            statusHTML +='<span class="text" style="font-size:18px; vertical-align:top; padding-left:3px">'+status.language+'</span>';
+                        }                
                     statusHTML +='</span>';
-                    statusHTML +='<span>';
-                        statusHTML +='<img src="https://img.icons8.com/material-rounded/22/4bffa5/time-machine.png" style="vertical-align:middle;"/>';
-                        statusHTML +='<span class="text" style="font-size:18px; vertical-align:top; padding-left:3px">'+ status.language+'</span>';
-                    statusHTML +='</span>';                    
+                    /*statusHTML +='<span class="con">';
+                        statusHTML +='<img src="https://img.icons8.com/material-rounded/22/4bffa5/time-machine.png" style="vertical-align:middle;"/>';                        
+                        if(status.language != null){                          
+                            statusHTML +='<span class="text" style="font-size:18px; vertical-align:top; padding-left:3px">'+ status.language+'</span>';
+                        }
+                    statusHTML +='</span>'; */
                 statusHTML +='</div>';            
             statusHTML +='</div>';
         statusHTML +='</div>';
@@ -37,4 +42,6 @@ request.onload = function() {
 request.send();
 
 //To find total commits
+//for direct count: https://api.github.com/repos/meetxpress/meetpatel.live-v-2.0/stats/contributors
 //request.open('GET', 'https://api.github.com/repos/meetxpress/meetpatel.live-v-2.0/commits', true)
+//console.log(lenght(data));
