d8be983cf73817d98b82095dfa1e58afed95e5c3
meetxpress <pmeet7895@gmail.com>
2020-12-11 15:50:11 +0530

    ..

--- js/gitRepo.js
+++ js/gitRepo.js
@@ -6,21 +6,21 @@ request.onload = function() {
     console.log(data);             
     $.each(data, function(i, status){        
         statusHTML +='<div class="service-col">';
-            statusHTML +='<div class="service-item content-box">';
+            statusHTML +='<div class="service-item content-box">';            
                 if(status.fork == true){
                     statusHTML +='<div><a href="'+status.html_url+'" target="_blank" style="text-decoration:none">';
-                        statusHTML +='<img src="https://img.icons8.com/ios-glyphs/25/4bffa5/code-fork.png" style="vertical-align:middle;"/>';
+                        statusHTML +='<img src="https://img.icons8.com/ios-glyphs/25/4bffa5/code-fork.png" title="Forked" style="vertical-align:middle;"/>';
                         statusHTML +='<span class="text" style="font-size:20px; vertical-align:top; font-weight: bold; color: #4bffa5; padding-left:3px">'+ status.name +'</span>';
                     statusHTML +='</a></div>';
                 } else{
                     statusHTML +='<div><a href="'+status.html_url+'" target="_blank" style="text-decoration:none">';
-                        statusHTML +='<img src="https://img.icons8.com/ios-filled/22/4bffa5/repository.png" style="vertical-align:middle;"/>';
+                        statusHTML +='<img src="https://img.icons8.com/ios-filled/22/4bffa5/repository.png" title="Repository" style="vertical-align:middle;"/>';
                         statusHTML +='<span class="text" style="font-size:20px; vertical-align:top; font-weight: bold; color: #4bffa5; padding-left:3px">'+ status.name +'</span>';
                     statusHTML +='</a></div>'; 
                 }                
                 statusHTML +='<div class="name">';
                     statusHTML +='<span style="margin-right: 15px;">';
-                        statusHTML +='<img src="https://img.icons8.com/ios-filled/22/4bffa5/source-code.png" style="vertical-align:middle;"/>';
+                        statusHTML +='<img src="https://img.icons8.com/ios-filled/22/4bffa5/source-code.png" title="Language" style="vertical-align:middle;"/>';
                         if(status.language == null){                            
                             statusHTML +='<span class="text" style="font-size:18px; vertical-align:top; padding-left:3px">Not Found</span>';
                         } else{
@@ -33,10 +33,10 @@ request.onload = function() {
                             statusHTML +='<span class="text" style="font-size:18px; vertical-align:top; padding-left:3px">'+ status.language+'</span>';
                         }
                     statusHTML +='</span>'; */
-                statusHTML +='</div>';            
+                statusHTML +='</div>';  
             statusHTML +='</div>';
         statusHTML +='</div>';
-    });
+    });        
     $('.service-items').html(statusHTML);
 }
 request.send();
